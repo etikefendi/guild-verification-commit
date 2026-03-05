e2f852133c7aa05f0d33a4f14fadcd38521815a0
Ömer Demir <omer.blunt@gmail.com>
2026-03-05 23:19:38 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:19:34.935Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T20:19:37.476Z
+Commit: Fix race condition in async operations
