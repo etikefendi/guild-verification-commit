c775be8381f7eaccda81ee7379ac7617932bc018
Ömer Demir <omer.blunt@gmail.com>
2026-03-05 23:20:29 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:20:26.897Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T20:20:28.284Z
+Commit: Fix memory leak in event handlers
