2fd0f8d6d64249d8ee04aea82605867d808b6934
Ömer Demir <omer.blunt@gmail.com>
2026-03-05 23:20:38 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:20:33.402Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T20:20:37.080Z
+Commit: Correct typo in configuration file
