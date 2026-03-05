9f42b2539ebc454b4a06de8b2796220a4be2bbb0
Ömer Demir <omer.blunt@gmail.com>
2026-03-05 23:19:36 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:19:29.605Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T20:19:34.935Z
+Commit: Fix CSS styling bug on mobile devices
