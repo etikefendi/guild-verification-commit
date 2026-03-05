af87f94553afce5449749d54dd1df34694a060d0
Ömer Demir <omer.blunt@gmail.com>
2026-03-05 23:20:44 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:20:35.811Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T20:20:43.345Z
+Commit: Resolve null pointer exception in user service
